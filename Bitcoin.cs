ae0919cfb6ab6948ded64d91b4b81d663fd0762b
SergiyMogiley <97164321+SergiyMogiley@users.noreply.github.com>
2023-02-04 16:38:08 +0200

    Add files via upload

---
+++ Bitcoin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ML.Data;
+// </SnippetAddUsing>
+
+namespace BitcoinAnomalyDetection
+{
+    // <SnippetDeclareTypes>
+    public class BitcoinData
+    {
+        [LoadColumn(0)]
+        public string Month;
+
+        [LoadColumn(1)]
+        public float numSales;
+    }
+
+    public class BitcoinPrediction
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(3)]
+        public double[] Prediction { get; set; }
+    }
+    // </SnippetDeclareTypes>
+}
